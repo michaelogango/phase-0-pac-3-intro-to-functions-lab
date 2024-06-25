2e248e3d146eb59d8b3cac5d0971d64258e0ba3e
Michael <michaelogango5@gmail.com>
2024-06-25 14:21:26 +0300

    added functions

---
+++ index.js
@@ -0,0 +1,35 @@
+const string="I can't hear you";
+const strings="HELLO";
+
+
+function shout(string){
+    return string.toUpperCase();    
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string){
+    console.log(string.toUpperCase());
+    console.log(string);
+}
+function logWhisper(string){
+console.log(strings.toLowerCase())
+console.log(strings)
+}
+
+function sayHiToHeadphonedRoommate(string){
+    // console.log(string);
+    // console.log(string.toLowerCase());
+    // console.log(string==string.toLowerCase());
+    if(string==toLowerCase()){
+        return "I can't hear you!"
+    }else if(string==toUpperCase()){
+        return "YES INDEED!"
+    }
+    else{
+        return "I would love to!"
+    }
+    
+  
+  
+}
